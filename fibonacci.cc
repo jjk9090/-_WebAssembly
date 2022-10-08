cb637f8eccbe0ad10e6983043a01895383811eb9
kookie <111575256+jjk9090@users.noreply.github.com>
2022-10-08 20:07:39 +0800

    first commit

---
+++ fibonacci.cc
@@ -0,0 +1,10 @@
+#include <emscripten.h>
+extern "C" {
+  EMSCRIPTEN_KEEPALIVE 
+  int fibonacci(int n) {
+    if(n < 2) {
+      return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
